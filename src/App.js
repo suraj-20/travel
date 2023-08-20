939c67f72cf5b9377936f7ae0d90894c65d5ddb7
Suraj <kumarsuraj54057@gmail.com>
2023-08-20 14:54:13 +0530

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
 
-function App() {
+const App = () => {
   return (
-   <>
-   Welcome
-   </>
+    <>
+      <Navbar />
+      
+    </>
   );
-}
+};
 
 export default App;
